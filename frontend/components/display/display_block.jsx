dacd1677ef6d66dec58c27a8d1334578d9d059a2
d-hebert <d.hebert@live.com>
2019-11-14 12:58:52 -0500

    Implemented scrolling

--- frontend/components/display/display_block.jsx
+++ frontend/components/display/display_block.jsx
@@ -81,6 +81,8 @@ class DisplayBlock extends React.Component {
                     {this.imageItem(3)}
                     {this.imageItem(4)}
                     {this.imageItem(5)}
+                    {this.imageItem(4)}
+                    {this.imageItem(3)}
                 </ul>
             )
         } else {
